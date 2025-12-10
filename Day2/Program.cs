bb20fc34bca270da9d575ef63bf1c0e60b211bf1
Truong Quoc Lap <truongquoclap3008@gmail.com>
2025-12-10 23:34:26 +0700

    Day 2

---
+++ Day2/Program.cs
@@ -0,0 +1,67 @@
+﻿using System.Text;
+using Common;
+
+List<Range> inputs = [];
+await FileReadHelper.ReadFileAsync("test2.txt", line =>
+{
+    foreach (var item in line.Split(","))
+    {
+        var temp = item.Split("-");
+        inputs.Add(new Range(long.Parse(temp[0]), long.Parse(temp[1])));
+    }
+});
+
+CountInvalidIds(inputs);
+
+return;
+
+void CountInvalidIds(List<Range> inputs)
+{
+    long result1 = 0;
+    long result2 = 0;
+
+    foreach (var (begin, end) in inputs)
+    {
+        for (long i = begin; i <= end; i++)
+        {
+            // or simply (s + s).Substring(1, 2 * s.Length - 2).Contains(s)
+            var (p1, p2) = IsRepeat(i);
+            result1 += p1;
+            result2 += p2;
+        }
+    }
+
+    Console.WriteLine(result1);
+    Console.WriteLine(result2);
+}
+
+(long, long) IsRepeat(long number)
+{
+    var numberAsString = number.ToString();
+    var len = numberAsString.Length;
+    var repeatSb = new StringBuilder(len / 2);
+    for (int repeatLen = 1; repeatLen <= len / 2; repeatLen++)
+    {
+        repeatSb.Append(numberAsString.AsSpan(repeatLen - 1, 1));
+
+        var textS = new StringBuilder(len);
+        var numberOfRepeat = len / repeatLen;
+        for (int i = 1; i <= numberOfRepeat; i++)
+        {
+            textS.Append(repeatSb);
+        }
+
+        if (textS.ToString() == numberAsString)
+        {
+            if (numberOfRepeat == 2) // part 1
+            {
+                return (number, number);
+            }
+            return (0, number);
+        }
+    }
+
+    return (0, 0);
+}
+
+public record Range(long Begin, long End);
--- README.md
+++ README.md
@@ -15,3 +15,20 @@ We can think that the arrow rotate is actually a cursor moving on a straight lin
 -   Simply add count every time we cross those 0 representers, include case where we cross 0 and minus 1 if it stops at a representers (since we already count it)
 
 ---
+
+# Day 2
+
+#### Part 1
+
+-   Simply check left half equal right half
+
+#### Part 2
+
+-   It is a common question in Math problem (I guess) - how to know if a string is a repeated pattern
+    -   One way is use very popular check, if that string is repeated, we just need to append it self and remove head tail to eliminate the chance it repeat as left half or right half. If that new string contain the original, it is a repeated pattern
+        ```
+        (s + s).Substring(1, 2 * s.Length - 2).Contains(s)
+        ```
+    -   The other way, which is what i use, is just pure brute force, find all posible pattern and build the new string to check if it is same with original.
+
+---
